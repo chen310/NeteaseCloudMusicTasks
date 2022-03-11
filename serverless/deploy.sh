99dab4f9f78afe6157aa10b2511087e2d6eee3b2
chen_10 <1803012703@qq.com>
2022-03-11 21:53:41 +0800

    fix: 部署后缺少配置文件

--- serverless/deploy.sh
+++ serverless/deploy.sh
@@ -3,7 +3,9 @@ if [ -z "$TENCENT_SECRET_ID" ] || [ -z "$TENCENT_SECRET_KEY" ]; then
 	echo -e "\033[1;31m部署失败 \033[0m"
 	exit 1
 fi
-url=$(python ./serverless/geturl.py)
+# 复制模板文件为配置文件
+cp config.example.json config.json
+url=$(python serverless/geturl.py)
 if [[ $url == *ERROR* ]]; then
 	# 未部署函数
 	if [[ $url == *ResourceNotFound* ]]; then
@@ -22,16 +24,16 @@ else
 	echo "正在解压"
 	unzip -o code.zip -d code/ >>/dev/null
 	rm -f code.zip
-	config_file="./config.json"
-	old_config_file="./config.old.json"
-	example_config_file="./config.example.json"
+	config_file="config.json"
+	old_config_file="config.old.json"
+	example_config_file="config.example.json"
 	# 复制模板文件为配置文件
 	cp $example_config_file $config_file
-	if [ -e "./code/config.json" ]; then
+	if [ -e "code/config.json" ]; then
 		# 备份配置文件
-		mv ./code/config.json $old_config_file
+		mv code/config.json $old_config_file
 		# 将旧配置文件中的数据转移到新配置文件中
-		python ./updateconfig.py $example_config_file $old_config_file $config_file
+		python updateconfig.py $example_config_file $old_config_file $config_file
 		if [ $? -ne 0 ]; then
 			echo "配置文件复制错误，请检查 config.json 文件是否填写正确"
 			echo -e "\033[1;31m部署失败 \033[0m"
@@ -50,12 +52,12 @@ echo "开始安装 ServerlessFramework"
 sudo npm install -g serverless >>/dev/null
 sudo mkdir tmp/
 shopt -s extglob
-sudo mv !(tmp|serverless|public|code|.github|.git|ql_update.py|__pycache__|Dockerfile|.dockerignore|scheduler.py) ./tmp
+sudo mv !(tmp|serverless|public|code|.github|.git|ql_update.py|__pycache__|Dockerfile|.dockerignore|scheduler.py) tmp
 
-python ./serverless/createyml.py
-sudo mv ./serverless.yml ./tmp
+python serverless/createyml.py
+sudo mv serverless.yml tmp
 
-cd ./tmp
+cd tmp
 echo "开始部署到腾讯云函数"
 result=$(sls deploy --debug)
 if [[ $result == *执行成功* ]]; then
