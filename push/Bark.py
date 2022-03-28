2fa47350774eeec3d6d9ef10314586e7085ed7e4
chen310 <33364396+chen310@users.noreply.github.com>
2022-03-28 16:17:32 +0800

    feat: bark 推送增加铃声、分组和图标设置 (#176)

--- push/Bark.py
+++ push/Bark.py
@@ -4,9 +4,9 @@ from urllib import parse
 
 def getKey(data):
     config = data['config']
-    if len(config['Bark_key']) == 0:
+    if len(config['Bark_key']) == 0 or len(config['Bark_url']) == 0:
         return None
-    return (config['module'], config['Bark_key'])
+    return (config['module'], config['Bark_key'], config['Bark_url'], config['sound'], config['group'], config['icon'])
 
 
 def push(title, mdmsg, mdmsg_compat, textmsg, config):
@@ -16,5 +16,12 @@ def push(title, mdmsg, mdmsg_compat, textmsg, config):
     if len(Bark_key) == 0 and len(Bark_url) == 0:
         return
     url = parse.urljoin(Bark_url, Bark_key)
+    data = {"title": title, "body": msg}
+    if config['sound']:
+        data['sound'] = config['sound']
+    if config['group']:
+        data['group'] = config['group']
+    if config['icon']:
+        data['icon'] = config['icon']
 
-    requests.post(url, data={"title": title, "body": msg})
+    requests.post(url, data=data)
