954db1f9faa15cbe88ac18755b2e17bd5a672b9d
chen_10 <1803012703@qq.com>
2022-04-18 09:59:32 +0800

    修改音乐人任务id

--- updateconfig.py
+++ updateconfig.py
@@ -38,8 +38,8 @@ def migrateTask(setting, template):
 
     if 'musician_task' in setting:
         musician_task = setting['musician_task']
-        kv = {'登录音乐人中心': '749006', '发布动态': '740004', '发布主创说': '755000', '回复粉丝评论': '732004', '回复粉丝私信': '755001',
-              '399000': '749006', '398000': '740004', '396002': '755000', '393001': '732004', '395002': '755001'}
+        kv = {'登录音乐人中心': '800004', '发布动态': '801005', '发布主创说': '755000', '回复粉丝评论': '732004', '回复粉丝私信': '807006',
+              '749006': '800004', '740004': '801005', '396002': '755000', '393001': '732004', '755001': '807006', '740005': '797005', '744005': '811003'}
         for key in kv:
             if key in musician_task and kv[key] in template['musician_task']:
                 musician_task[kv[key]] = updateConfig(musician_task[key], template['musician_task'][kv[key]])
